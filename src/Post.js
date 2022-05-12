4425ee593441f6bb186833be5e0fbbb8a6dcbbd7
HarshaVardhanReddyDuvvuru <35846806+HarshaVardhanReddyDuvvuru@users.noreply.github.com>
2022-05-12 22:04:38 +0530

    Post With text completed

--- src/Post.js
+++ src/Post.js
@@ -1,33 +1,50 @@
 import { Component } from "react";
 import "./post.css";
 
-const onChangeText = (e) => {
-  this.setState({
-    post_input: e.target.value
-  });
-};
-
 class Post extends Component {
   state = {
-    post_input: ""
+    post_input: "",
+    post_list: []
+  };
+
+  onChangeText = (e) => {
+    this.setState({
+      post_input: e.target.value
+    });
+  };
+
+  updatePosts = () => {
+    const { post_input, post_list } = this.state;
+    this.setState({
+      post_list: [post_input, ...post_list]
+    });
   };
 
   render() {
-    const post_input = this.state;
+    const { post_input, post_list } = this.state;
+    console.log(post_list);
     return (
-      <div className="profile-container">
-        <img
-          src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
-          className="profile-img"
-        />
-        <input
-          type="text"
-          id="firstName"
-          placeholder="Write something Here!"
-          onChange={this.onChangeText}
-          className="input-el"
-        />
-      </div>
+      <>
+        <div className="profile-container">
+          <img
+            src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
+            className="profile-img"
+          />
+          <input
+            type="text"
+            id="firstName"
+            placeholder="Write something Here!"
+            onChange={this.onChangeText}
+            className="input-el"
+          />
+        </div>
+        <button onClick={this.updatePosts}>Post</button>
+        <ul>
+          {post_list.map((e) => {
+            return <li>{e}</li>;
+          })}
+        </ul>
+      </>
     );
   }
 }
