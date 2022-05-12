cb19d524a3a9f055a71ed0a6d2ea780904c87fe1
HarshaVardhanReddyDuvvuru <35846806+HarshaVardhanReddyDuvvuru@users.noreply.github.com>
2022-05-12 21:15:40 +0530

    Testing

---
+++ src/Post.js
@@ -0,0 +1,23 @@
+import { Component } from "react";
+
+import TodoItem from "../TodoItem";
+
+import "./index.css";
+
+const initialTodosList = [];
+
+class Post extends Component {
+  state = {};
+
+  render() {
+    return (
+      <div className="">
+        <div className="">
+          <h1 className="">Simple Todos</h1>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Post;
