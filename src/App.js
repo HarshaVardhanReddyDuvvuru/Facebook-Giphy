0113957edf3acd5c6754f019bbf574ac375c6dc3
HarshaVardhanReddyDuvvuru <35846806+HarshaVardhanReddyDuvvuru@users.noreply.github.com>
2022-05-12 21:38:27 +0530

    Post Image

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import "./styles.css";
+import Post from "./Post";
 
 export default function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Post />
+    </div>
+  );
 }
--- src/Post.js
+++ src/Post.js
@@ -1,20 +1,32 @@
 import { Component } from "react";
+import "./post.css";
 
-import TodoItem from "../TodoItem";
-
-import "./index.css";
-
-const initialTodosList = [];
+const onChangeText = (e) => {
+  this.setState({
+    post_input: e.target.value
+  });
+};
 
 class Post extends Component {
-  state = {};
+  state = {
+    post_input: ""
+  };
 
   render() {
+    const post_input = this.state;
     return (
-      <div className="">
-        <div className="">
-          <h1 className="">Simple Todos</h1>
-        </div>
+      <div className="profile-container">
+        <img
+          src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
+          className="profile-img"
+        />
+        <input
+          type="text"
+          id="firstName"
+          placeholder="Write something Here!"
+          onChange={this.onChangeText}
+          className="input-el"
+        />
       </div>
     );
   }
